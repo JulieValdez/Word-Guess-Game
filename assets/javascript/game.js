9f5d4e4ee313d5bd7c8a7d2086f32046a9bf9932
JulieValdez <juliegragg0605@gmail.com>
2020-01-13 15:50:48 -0800

    HTML written

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//create variable for the words to be used
+const currentWord = [
+    { q: "_"+"_"+"_"+"_"+"_"+"_", a: "i"+"o"+"c"+"a"+"n"+"e"}
+]
+//create variable for hints to be used
+const currentWordHint = [
+    {"This powder is used in a battle of wits!"}
+]
