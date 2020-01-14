68c6383a2ecf7eddc5c35babd20b4896e0d361b3
JulieValdez <juliegragg0605@gmail.com>
2020-01-13 21:20:54 -0800

    beginning js functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,63 @@
+//VARIABLES
+//===============================================================
+
 //create variable for the words to be used
-const currentWord = [
-    { q: "_"+"_"+"_"+"_"+"_"+"_", a: "i"+"o"+"c"+"a"+"n"+"e"}
-]
+const currentWord = ["iocane", "buttercup", "max", "westley"];
+
 //create variable for hints to be used
 const currentWordHint = [
-    {"This powder is used in a battle of wits!"}
-]
+  "odorless, tasteless, and dissolves instantly in liquid...",
+  "title character",
+  "miracle worker",
+  "Dread Pirate Robert"
+];
+
+//variable for score, start with score of 0
+const score = 0;
+
+//variable to hold the index of the current word
+const currentWordIndex = 0;
+
+//variable to hold the index of the current word hint
+const currentHintIndex = 0;
+
+//variable for guesses remaining, give them 10
+const guessesRemaining = 12;
+
+//variable for letters already guessed
+const lettersGuessed = [];
+
+//FUNCTIONS
+//=============================================================
+
+//function to start game
+document.onkeyup = function(event){
+    const userGuess = event.key;
+}
+//function to render questions
+function renderQuestion() {
+    if (currentWordIndex <= (currentWord - 1)) {
+     document.querySelector("#currentWord").innerHTML = currentWord[currentWordIndex];   
+    } else {
+        
+    }
+}
+
+//function to create empty answer array - for loop
+const answerArray = [];
+for (let index = 0; index < currentWord.length; index++) {
+  answerArray[index] = "_";
+}
+//keeps track of remaining letter in word being played so when it gets to 0 game is won
+const remainingLetters = currentWord.length;
+
+//function to push the letters guessed to the variable "lettersGuessed" update html id="lettersGuessed"
+function lettersGuessed() {
+    if (condition) {
+        
+    }
+    
+}
+//function to
+
+renderQuestion ()
