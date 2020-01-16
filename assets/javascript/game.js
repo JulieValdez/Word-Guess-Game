fce002314f533aa38244ea13d98e9766e8617a04
JulieValdez <juliegragg0605@gmail.com>
2020-01-15 22:56:26 -0800

    last commit :(

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,10 @@ let listOfWords = [
   "max", 
   "westley"
 ];
-let currentWord = listOfWords[0]
+let currentWord = listOfWords[0];
+
+//variable for underscores to be displayed
+let wordUnderscore = (currentWord.length).join("_");
 
 //create variable for hints to be used
 let listOfWordHints = [
@@ -17,7 +20,7 @@ let listOfWordHints = [
   "miracle worker",
   "Dread Pirate Robert"
 ];
-let currentWordHint = listOfWordHints[0]
+let currentWordHint = listOfWordHints[0];
 
 //variable for wins, start with score of 0
 let wins = 0;
@@ -25,65 +28,34 @@ let wins = 0;
 //variable for guesses remaining, give them 12
 let guessesRemaining = 12;
 
+//Functions:
+function startGame() {
+  document.getElementById("#currentWord").innerHTML = wordUnderscore
+}
+
 //on first key press game starts
 //first word is chosen from list of words array
+//length of word is determined and displayed with underscores in html id
 //first hint is chosen from list of word hints array
+//hint is displayed in html id
 //user chooses a letter
+document.onkeyup = function(event) {
+  var userGuess = event.key
+
+  if (currentWord[i].includes(userGuess)) {
+    
+  }
+}
+//letter chosen is stored in variable
 //if letter is included in current word it is pushed to appropriate index position
 //display chosen letter in html id
 //if it is not included in current word it is pushed to letter guessed array
 //display incorrect letter guessed in html id
 //count of guesses remaining goes down
 //if all the index places in current word are populated the word is complete and the user won
-//
+//next word in the index is displayed if user wins or remaining guesses are at 0
 
-
-//FUNCTIONS
-//=============================================================
-// on first key press game starts
-document.onkeyup = function(event) {
-  var userGuess = event.key;
-  console.log("missed work");
-  if (currentWord.includes(userGuess)) {
-    console.log("worked");
-    
-  } else {
-    
-  }
-  
-}
-//function to render words in variable
-function renderCurrentWord() {
-    if (currentWordIndex <= (currentWord - 1)) {
-     document.querySelector("#currentWord").innerHTML = currentWord[currentWordIndex];   
-    } else {
-      document.querySelector("#currentWord").innerHTML = "Game Over!";
-    }
-}
-
-//function to create empty answer array - for loop
-let answerArray = [];
-for (let index = 0; index < currentWord.length; index++) {
-  answerArray[index] = "_";
-}
-//keeps track of remaining letter in word being played so when it gets to 0 game is won
-let remainingLetters = currentWord.length;
+startGame();
 
 
 
-// while (remainingLetters > 0) {
-//   document.getElementById("currentWord").innerHTML = answerArray.join(" ");
-// }
-
-// renderCurrentWord()
-
-// word is chosen from currentWord array and blank dashes are displayed
-// corresponding hint is displayed in empty <p>
-
-// player hits a key, if it is part of the currentWord it is diplayed in the correct place
-// if key is not correct, it is placed in the lettersGuessed div
-
-//track the number of guesses
-
-//if the word is solved - alert the player they have won and play audio file and display image
-//else - alert player they have lost and play audio file
